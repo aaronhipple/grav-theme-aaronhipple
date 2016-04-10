3f8e556553edf1f557ed758665140a8de98b284c
Aaron Hipple <aaron@footstepsmarketing.com>
2016-04-10 15:11:29 -0600

    Gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    compass = require('gulp-compass'),
+    concat = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps'),
+    fs = require('fs');
+
+gulp.task('compile', function () {
+    return [
+        gulp.src('scss/*.scss')
+        .pipe(compass({
+            css: 'css',
+            sass: 'scss',
+            environment: 'development',
+            sourcemap: true,
+            style: 'expanded'
+        }))
+        .pipe(gulp.dest('tmp'))
+    ]
+})
+
+gulp.task('watch', function () {
+    var watcher = gulp.watch(
+        [
+            'js/*.js',
+            'scss/*.scss'
+        ], ['compile']
+    );
+})
